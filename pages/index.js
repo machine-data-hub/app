5a218dd69a826b899327bdda2aa0934b69292a55
ceciliabarnes <cbarnes7@uw.edu>
2021-01-20 22:37:09 -0800

    testing

--- pages/index.js
+++ pages/index.js
@@ -10,22 +10,22 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to the PHM Data Hub!!
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Soon there will be cool datasets here.
         </p>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>About Us &rarr;</h3>
+            <p>Find in-depth data about prognostics and meet our team.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
             <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Learn about our Python Client and how to use it!</p>
           </a>
 
           <a
