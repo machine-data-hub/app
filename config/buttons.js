5104aca11060a6e0eff0e91dc8294b9a4a1fda32
mattsul <66149825+mattsul@users.noreply.github.com>
2021-02-01 23:07:59 -0800

    Add files via upload

---
+++ config/buttons.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+const navButtons = [
+
+    {
+        label: "About Us",
+        path: "/about-us",
+        icon: ""
+    },
+    {
+        label: "Settings",
+        path: "/settings",
+        icon: ""
+    },
+    {
+        label: "Home Page",
+        path: "/ ",
+        icon: ""
+    }
+
+];
+
+export default navButtons;
---
+++ config/carddata.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+const carddata = [
+
+    {
+        title: "Test 1",
+        description: "Description 1",
+        tags: "tag 1"
+    },
+    {
+        title: "Test 2",
+        description: "Description 2",
+        tags: "tag 2"
+    },
+    {
+        title: "Test 3",
+        description: "Description 3",
+        tags: "tag 3"
+    }
+
+];
+
+export default carddata;
