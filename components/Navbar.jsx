b5c89816b4524b0492235fb1c36315e02cce40ab
mattsul <66149825+mattsul@users.noreply.github.com>
2021-03-12 18:30:50 -0800

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,26 +1,44 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-    
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 const Navbar = () => {
-    const router = useRouter()
-    
-    return (
-        <div className="navbar">
-            <div className="container">
-                <div className="navbar__left">
-                    <Link href="/"><h1>Machine Data Hub</h1></Link>
-                </div>
-                <div className="navbar__right">
-                    <ul>
-                        {/* if the condition returns true -> give a nav-active class to li element */}
-                        <Link href="/"><li className={router.pathname === '/' ? 'nav-active' : ''}>Home</li></Link>
-                        <Link href="/about"><li className={router.pathname === '/about' ? 'nav-active' : ''}>About</li></Link>
-                        <Link href="/suggest-dataset"><li className={router.pathname === '/suggest-dataset' ? 'nav-active' : ''}>Suggest a Dataset</li></Link>
-                    </ul>
-                </div>
-            </div>
+  const router = useRouter();
+
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="navbar__left">
+          <Link href="/">
+            <h1>Machine Data Hub</h1>
+          </Link>
         </div>
-    );
-}
-    
-export default Navbar
+        <div className="navbar__right">
+          <ul>
+            {/* if the condition returns true -> give a nav-active class to li element */}
+            <Link href="/">
+              <li className={router.pathname === "/" ? "nav-active" : ""}>
+                Home
+              </li>
+            </Link>
+            <Link href="/about">
+              <li className={router.pathname === "/about" ? "nav-active" : ""}>
+                About
+              </li>
+            </Link>
+            <Link href="/suggest-dataset">
+              <li
+                className={
+                  router.pathname === "/suggest-dataset" ? "nav-active" : ""
+                }
+              >
+                Suggest a Dataset
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
