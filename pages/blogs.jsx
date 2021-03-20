8619ffb5a53572e21009fc893a60e0135aab615e
mattsul <66149825+mattsul@users.noreply.github.com>
2021-03-19 19:53:12 -0700

    Start of blog homepage

---
+++ pages/blogs.jsx
@@ -0,0 +1,18 @@
+import blogs from "../data/blogdata.json";
+import Link from "next/link";
+import React from 'react'
+
+export default function BlogHome( props ) {
+    return (
+        <>
+        <h1>Blog Pages!: {props.blogTitle}</h1> 
+        <Link href={`/blogs/${props.blogTitle}`} />
+        </>
+    )
+}
+
+BlogHome.getInitialProps = () => {
+    return {
+        blogTitle: "Test inside Home.getInitialProps"
+    }
+}
