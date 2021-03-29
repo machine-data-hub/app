f5a27bc1be05a08b98a9a2be04f99b3216748df1
mattsul <66149825+mattsul@users.noreply.github.com>
2021-03-29 09:20:38 -0700

    Unfinished Blog Page
    Routing needs attention

--- pages/blogs.jsx
+++ pages/blogs.jsx
@@ -1,18 +1,40 @@
-// import blogs from "../data/blogdata.json";
 import Link from "next/link";
 import React from 'react'
+import fs  from 'fs';
+import matter from "gray-matter";
 
-import Layout from "../components/Layout";
-import teams from "../data/team.json";
+export default function BlogHome( props ) {
+    return (
+        <>
+        <h1>Blog Pages!: {props.blogTitle}</h1> 
+        <Link href={`/blogs/${props.blogTitle}`} />
+        </>
+    )
+}
 
-const BlogHome = () => {
-  return (
-    <Layout title="Blogs | Machine Data Hub">
-      <div>
-        Blogs Home
-      </div>
-    </Layout>
-  );
-};
+export async function getStaticProps() {
+    const allFiles = fs.readdirSync(`${process.cwd()}/blogcontent/posts`);
 
-export default BlogHome;
+    const posts = files.map((filename) => {
+        const markdownWithMetadata = fs.readFileSync(`content/posts/${filename}`).toString();
+
+        const {data} = matter(markdownWithMetadata);
+
+        const options = {year: "numeric", month:"long", day:"numeric"};
+        const formmattedDate = date.date.toLocaleDateString('en-Us', options);
+
+        const frontmatter = {
+            ...data,
+            date: formmattedDate,
+        };
+
+        return {
+            slug: filename.replace(".md", "",),
+            frontmatter,
+        };
+    });
+
+    return {
+        props: {posts,},
+    };
+}
