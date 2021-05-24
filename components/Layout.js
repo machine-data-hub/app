2692c67392ddec67c0fd7d34ec10008ee260e034
sullimat@uw.edu <1UWMattswim!!>
2021-05-24 11:44:07 -0700

    Meta data 2

--- components/Layout.js
+++ components/Layout.js
@@ -21,6 +21,11 @@ const Layout = ({ children, title }) => {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#2178b3" />
+
+        <meta property="og:url"   content="https://machinedatahub.ai" />
+        <meta property="og:type"  content="website" />
+        <meta property="og:title" content="Machine Data Hub" />
+        <meta property="og:description" content="Open source data hub for prognostics and health management datasets"/>
       </Helmet>
       <Navbar />
       <div className="container">{children}</div>
