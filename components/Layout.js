e591152ccef69f7fc0ae4f83005f317275839821
sullimat@uw.edu <1UWMattswim!!>
2021-05-24 11:48:13 -0700

    Meta tags 3

--- components/Layout.js
+++ components/Layout.js
@@ -7,26 +7,32 @@ const Layout = ({ children, title }) => {
     <>
       <Head>
         <title>{title}</title>
-      </Head>
-      <Helmet>
-        <meta name="application-name" content="Machine Data Hub" />
-        <meta name="author" content="Machine Data Hub" />
-        <meta
-          name="description"
-          content="Open source data hub for locating prognostics and health management datasets"
-        />
-        <meta
-          name="keywords"
-          content="Machine learning, datasets, PHM, prognostics, health management, data hub, open source"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="theme-color" content="#2178b3" />
+        <Helmet>
+          <meta name="application-name" content="Machine Data Hub" />
+          <meta name="author" content="Machine Data Hub" />
+          <meta
+            name="description"
+            content="Open source data hub for locating prognostics and health management datasets"
+          />
+          <meta
+            name="keywords"
+            content="Machine learning, datasets, PHM, prognostics, health management, data hub, open source"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="theme-color" content="#2178b3" />
 
-        <meta property="og:url"   content="https://machinedatahub.ai" />
-        <meta property="og:type"  content="website" />
-        <meta property="og:title" content="Machine Data Hub" />
-        <meta property="og:description" content="Open source data hub for prognostics and health management datasets"/>
-      </Helmet>
+          <meta property="og:url" content="https://machinedatahub.ai" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Machine Data Hub" />
+          <meta
+            property="og:description"
+            content="Open source data hub for prognostics and health management datasets"
+          />
+        </Helmet>
+      </Head>
       <Navbar />
       <div className="container">{children}</div>
       <div className="footer"></div>
